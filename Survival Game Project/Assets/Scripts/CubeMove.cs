0601b877028628f439531b751e9cd61e66e698c3
Kesavan Shan <kns_8101@hotmail.com>
2020-03-29 03:41:03 -0400

    Added Moving Cube

---
+++ Survival Game Project/Assets/Scripts/CubeMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    public void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0.0f, moveSpeed * Time.deltaTime, 0.0f)); //move up
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0.0f, -moveSpeed * Time.deltaTime, 0.0f)); //move down
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-moveSpeed * Time.deltaTime, 0.0f, 0.0f)); //move left
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0.0f, 0.0f)); //move right
+        }
+    }
+}
